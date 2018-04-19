2cad00e49a5db9a50f6cc9d19166f8e4e6237000
Victor Freixes <vfreixes@gmail.com>
2018-04-19 17:04:37 +0200

    GitIgnore updated Demo content

---
+++ SideScroller/Intermediate/Build/Win64/SideScroller/Development/UELinkerFixups/PCH.UELinkerFixups.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/Alumno/Desktop/CasinoRoyale/SideScroller/Intermediate/Build/Win64/SideScroller/Development/UELinkerFixups/PCH.UELinkerFixups.h"
---
+++ SideScroller/Intermediate/Build/Win64/SideScroller/Development/UELinkerFixups/PCH.UELinkerFixups.h
@@ -0,0 +1,2 @@
+// PCH for C:\Users\Alumno\Desktop\CasinoRoyale\SideScroller\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h
+#include "C:\Users\Alumno\Desktop\CasinoRoyale\SideScroller\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h"
